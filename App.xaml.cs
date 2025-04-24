c277af46d7930e62e22df3c15b550895d7272ca1
Yupi <youngperchik2404@gmail.com>
2025-04-24 19:11:20 +0500

    Created a part of homework on WPF

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace First_WPF_Practice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
